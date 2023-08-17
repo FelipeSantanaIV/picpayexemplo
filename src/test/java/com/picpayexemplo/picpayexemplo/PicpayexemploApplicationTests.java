ae0c9ebe98e80c279397b913b0ff10bd04ee4660
Felipe Sant'Ana <felipe.santana1995@hotmail.com>
2023-08-17 11:32:05 -0300

    Primeiro commit

---
+++ src/test/java/com/picpayexemplo/picpayexemplo/PicpayexemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.picpayexemplo.picpayexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpayexemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
